64e781195c3d590e6982a8af2a5778a2c323c064
Jeff Ho <jeffho1992@gmail.com>
2025-06-03 23:41:31 +0100

    Build CarModel and CarMake Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,12 +1,14 @@
-# from django.contrib import admin
+from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
 
 # CarModelAdmin class
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # CarMakeAdmin class with CarModelInline
 
